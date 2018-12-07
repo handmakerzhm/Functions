dbd1b5696f9908fe36fdc23f476aa37c52fd5096
Wane Smith <wanesmith7@gmail.com>
2018-12-06 23:03:01 -0500

    file changes

--- build/javascript/jscript3.js
+++ build/javascript/jscript3.js
@@ -5,7 +5,7 @@
 //     title:'Stage Setting:',
 //     stage:['The System','The Protocol','Security'],
 //     defineStage (passP){
-//         console.log(this.stage,passP)
+//         console.log(this.stage, passP)
 //     }
 // }
 
@@ -13,8 +13,10 @@
 
 //------------------------------------
 //Example1-1: with a forEach loop
-/*Note: The this at the end of function 
-is references outside the function method*/
+/*Note: The (this) at the end of function 
+is references outside the function method.
+Choose to use the template literal is not
+necessary, just experimenting*/
 
 // const scene2 = {
 //     title:'Stage Setting:',
